52370189dd89e6fe79f7640effba7ecccb6e1241
elias <you@example.com>
2025-03-12 12:20:43 +0100

    Start_React_configuration

---
+++ 5cwi_demo/app/cars/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+export default function cars({}: Props) {
+  return (
+    <div>cars</div>
+  )
+}
---
+++ 5cwi_demo/app/not-found.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+const Notfound = (props: Props) => {
+  return (
+    <div>We have a problem</div>
+  )
+}
